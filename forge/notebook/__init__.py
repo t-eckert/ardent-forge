af86de99cfc524932daa7b81ffdf63cfdd4ee6a5
Thomas Eckert <thomas.james.eckert@gmail.com>
2026-04-12 13:03:04 -0400

    feat(notebook): export writer from package

--- forge/notebook/__init__.py
+++ forge/notebook/__init__.py
@@ -1,4 +1,11 @@
 from forge.notebook.errors import NotebookWriteError
 from forge.notebook.reader import NotebookReader, SearchHit
+from forge.notebook.writer import ALLOWED_WRITE_PREFIXES, NotebookWriter
 
-__all__ = ["NotebookReader", "NotebookWriteError", "SearchHit"]
+__all__ = [
+    "ALLOWED_WRITE_PREFIXES",
+    "NotebookReader",
+    "NotebookWriteError",
+    "NotebookWriter",
+    "SearchHit",
+]
